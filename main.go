d65686e7a48ed623ba2ad4acc27496c91aa86d5f
markliby <marklib8681@gmail.com>
2022-12-08 16:09:11 +0800

    add aliyun deploy

--- main.go
+++ main.go
@@ -5,40 +5,36 @@ package main
 
 import (
 	"fmt"
-	"os/exec"
-	"runtime"
-	"strings"
-	"time"
 
 	"kusionstack.io/kcl-playground/pkg/play"
 )
 
 func main() {
-	addr := "localhost:2022"
+	addr := ":80"
 	fmt.Printf("listen at http://%s\n", addr)
 
-	go func() {
-		time.Sleep(time.Second * 2)
-		openBrowser(addr)
-	}()
+	// go func() {
+	// 	time.Sleep(time.Second * 2)
+	// 	openBrowser(addr)
+	// }()
 
 	play.Run(addr, &play.Option{
 		PlayMode: true,
 	})
 }
 
-func openBrowser(url string) error {
-	if !strings.HasPrefix(url, "http") {
-		url = "http://" + url
-	}
-	switch runtime.GOOS {
-	case "linux":
-		return exec.Command("xdg-open", url).Start()
-	case "windows":
-		return exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
-	case "darwin":
-		return exec.Command("open", url).Start()
-	default:
-		return fmt.Errorf("unsupported platform")
-	}
-}
+// func openBrowser(url string) error {
+// 	if !strings.HasPrefix(url, "http") {
+// 		url = "http://" + url
+// 	}
+// 	switch runtime.GOOS {
+// 	case "linux":
+// 		return exec.Command("xdg-open", url).Start()
+// 	case "windows":
+// 		return exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
+// 	case "darwin":
+// 		return exec.Command("open", url).Start()
+// 	default:
+// 		return fmt.Errorf("unsupported platform")
+// 	}
+// }
