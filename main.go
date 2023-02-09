a367d2510d5b8cc28e71d8d44f74ed9a87dcfdfe
Peefy <xpf6677@163.com>
2023-02-09 19:33:55 +0800

    refactor: deploy code

--- main.go
+++ main.go
@@ -13,28 +13,7 @@ func main() {
 	addr := ":2022"
 	fmt.Printf("listen at http://%s\n", addr)
 
-	// go func() {
-	// 	time.Sleep(time.Second * 2)
-	// 	openBrowser(addr)
-	// }()
-
 	play.Run(addr, &play.Option{
 		PlayMode: true,
 	})
 }
-
-// func openBrowser(url string) error {
-// 	if !strings.HasPrefix(url, "http") {
-// 		url = "http://" + url
-// 	}
-// 	switch runtime.GOOS {
-// 	case "linux":
-// 		return exec.Command("xdg-open", url).Start()
-// 	case "windows":
-// 		return exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
-// 	case "darwin":
-// 		return exec.Command("open", url).Start()
-// 	default:
-// 		return fmt.Errorf("unsupported platform")
-// 	}
-// }
